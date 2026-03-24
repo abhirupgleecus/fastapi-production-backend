dab916c3c1cd6da941f87b936c48f06646aabf38
Abhirup Basu <abhirup.basu@gleecus.com>
2026-03-24 19:27:56 +0530

    feat: auto create tables on startup

--- app/main.py
+++ app/main.py
@@ -10,8 +10,17 @@ from app.core.exceptions import (
     UnauthorizedException,
 )
 
+from app.db.base import Base
+from app.db.session import engine
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def on_startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
 
 app.include_router(api_router, prefix="/api/v1")
 
