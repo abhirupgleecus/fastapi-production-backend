b62c6655d815607d02a0566fca8c608d5c6d0ddb
Abhirup Basu <abhirup.basu@gleecus.com>
2026-03-24 18:30:47 +0530

    Re-commit after Black fail

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import Field
 from functools import lru_cache
 
@@ -18,9 +18,11 @@ class Settings(BaseSettings):
         ..., env="JWT_ACCESS_TOKEN_EXPIRE_MINUTES"
     )
 
-    class Config:
-        env_file = ".env"
-        case_sensitive = True
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        case_sensitive=True,
+        extra="ignore",  # 👈 THIS prevents crash
+    )
 
 
 @lru_cache()
